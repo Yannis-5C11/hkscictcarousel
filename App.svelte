86f5f2b9bf184a69412d7da024e27aa19397da6f
Yannis-5C11 <92708549+Yannis-5C11@users.noreply.github.com>
2021-10-27 16:49:18 +0800

    Uplode Carousel

--- App.svelte
+++ App.svelte
@@ -1,5 +1,6 @@
 <script>
-	import Button from "./Button.svelte";
+  import Button from "./Button.svelte";
+  import Carousel from "./Carousel.svelte";
 </script>
 
 <style>
@@ -7,10 +8,29 @@
     font-family: sans-serif;
     text-align: center;
   }
+  .slide-content img {
+    height: 50vh;
+  }
 </style>
 
 <main>
 	<h1>Hello CodeSandbox</h1>
 	<h2>Start editing to see some magic happen!</h2>
 	<Button />
+  <br>
+  <br>
+  <Carousel perPage="1">
+  <div class="slide-content">
+  <img alt="1" src="https://i.imgur.com/cHNuyQr.jpeg">
+ </div>
+  <div class="slide-content">
+  <img alt="2" src="https://i.imgur.com/Izh5ZHd.jpg">
+  </div>
+  <div class="slide-content">
+   <img alt="3" src="https://i.imgur.com/5V73tPG.jpg">
+  </div>
+  <div class="slide-content">
+   <img alt="4" src="https://i.imgur.com/uSKu6Fl.jpg">
+  </div>
+</Carousel>
 </main>
